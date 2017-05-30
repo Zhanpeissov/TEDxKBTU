6e527f5c788a45298e8cefe2bc10c8bdb7642507
daniyarka <daniyar_k-98@mail.ru>
2017-05-31 03:00:35 +0600

    full

--- TEDxKBTU/TEDxKBTU/Myjs.js
+++ TEDxKBTU/TEDxKBTU/Myjs.js
@@ -1,24 +1,17 @@
 ﻿$(document).ready(function () {
-    // Add smooth scrolling to all links in navbar + footer link
     $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
 
-            // Store hash
+            event.preventDefault();
             var hash = this.hash;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 900, function () {
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        } // End if
+        } 
     });
 
     $(window).scroll(function () {
@@ -32,10 +25,71 @@
         });
     });
 })
-function myMap() {
-    var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-    var mapProp = { center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId: google.maps.MapTypeId.ROADMAP };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    var marker = new google.maps.Marker({ position: myCenter });
-    marker.setMap(map);
+window.onload = function () {
+    console.log("u", localStorage.getItem("username"));
+    if (localStorage.getItem("username") != null && localStorage.getItem("password") != null) {
+        $("#container").load('Account.html');
+    }
 }
+window.onload = function () {
+    console.log("u", localStorage.getItem("username"));
+    if (localStorage.getItem("username") != null && localStorage.getItem("password") != null) {
+        $("#container").load('Account.html');
+    }
+    var callModal = document.getElementById('callModal');
+    var modal = document.getElementById('myFirstModal');
+    var closeButton = document.getElementById('closeButton');
+    callModal.onclick = function () {
+        $('body').css ({
+            "overflow": "hidden"
+        });
+        modal.style.display = 'block';
+    };
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            $('body').css({
+                "overflow": ""
+            });
+            modal.style.display = 'none';
+        }
+    }
+    closeButton.onclick = function () {
+        $('body').css({
+            "overflow": ""
+        });
+        modal.style.display = 'none';
+    }
+};
+
+$('.sbtn').click(function () {
+    document.location.href = "Signin.html";
+})
+
+$('.sub').click(function () {
+    
+    alert("Your mennage has been sent");
+});
+var user = "username";
+var password = "password";
+$('#reg').click(function (event) {
+    event.preventDefault();
+    var email = $('#email').val();
+    var pass = $('#pass').val();
+    localStorage.setItem(user, email);
+    localStorage.setItem(password, pass);
+})
+$('#in').click(function (event) {
+    event.preventDefault();
+    var i,j;
+    var email = $('#mail').val();
+    var pass = $('#pwd').val();
+    if (localStorage.getItem("username") != null) {
+        $("#container").load('Account.html');
+    }
+    else alert("You're not registered");
+})
+$('#accx').click(function () {
+    $("#container").load("Account2.html");
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+})
